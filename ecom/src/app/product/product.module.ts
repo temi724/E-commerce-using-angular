caba75c282b0d40357433dfa61a5d19889756af9
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-04-19 01:36:10 +0100

    Changes to be committed: 	modified:   ecom/src/app/product/product-list/product-list.component.css 	modified:   ecom/src/app/product/product-list/product-list.component.html 	modified:   ecom/src/app/product/product.module.ts 	modified:   ecom/src/styles.css

--- ecom/src/app/product/product.module.ts
+++ ecom/src/app/product/product.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
 })
 export class ProductModule {}
